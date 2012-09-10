899c3c770d622d1f4a607579475485b5f35074fb
taolee <daujin@gmail.com>
2012-09-10 18:10:22 +0200

    pullreq stuff

--- app/controllers/Config.scala
+++ app/controllers/Config.scala
@@ -7,15 +7,16 @@ object Config {
   val gitHubUser = "taolee"
   val gitHubPassword = "taolee123"
   val gitHubRepo = "scalahooks"
-  val hookUrl = "http://scalahooks.herokuapp.com/githubMsg"
+  //val hookUrl = "http://scalahooks.herokuapp.com/githubMsg"
   val gitHubUrl = "https://api.github.com/repos/" + gitHubUser + "/" + gitHubRepo
-  //val hookUrl = "http://requestb.in/1imhe4b1"
+  val hookUrl = "http://requestb.in/1imhe4b1"
  
-  val reviewerList = List("@taolee", "@adriaan___", "@odersky___", "@lrytz___", "@heather___", "@vlad___")
+  val reviewerList = List("taolee", "adriaan___", "odersky___", "lrytz___", "heather___", "vlad___")
   val reviewMsgList = List("Review", "review", "REVIEW")
   val reviewedMsgList = List("LGTM", "lgtm")
   val defaultLabelList = List("tested", "reviewed")
-  
+  //val buildBot = "scala-jenkins"
+  val buildBot = "taolee"
 
   val waitMinInterval = 12 * 48 // 48 hours = (12 * 5) * 48 minutes
   val waitHourInterval = 6
@@ -24,6 +25,6 @@ object Config {
   val initialDelay = 1 minutes
   val enableActor = true
   
-  val pullRequestMileStone = Map("taolee:master" -> "RC1", 
-                                 "taolee:heroku" -> "RC0")
+  val pullRequestMilestoneMap = Map("taolee:master" -> "RC1", 
+                                    "taolee:heroku" -> "RC0")
 }
